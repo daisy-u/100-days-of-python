0886e3a57b576bf595f504b1609a94a6076d2e9d
Daisy Umotong <60119798+daisy-u@users.noreply.github.com>
2021-02-26 12:09:11 -0800

    Delete inputexercise.py

--- inputexercise.py
+++
@@ -1,6 +0,0 @@
-#Write a program that prints the number of characters in a user's name.
-#Write your code below this line 👇
-name = input('What is your name?')
-print(len(name))
-#OR
-print(len(input("What is your name? ")))
