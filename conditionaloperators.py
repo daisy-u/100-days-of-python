d97240263843a9be54fb44754c022764cb3cf774
Daisy Umotong <x4daisy@rocketmail.com>
2021-03-22 18:31:40 +0000

    comparison and conditional operators

---
+++ conditionaloperators.py
@@ -0,0 +1,10 @@
+# Comparison Operators
+# > Greater Than
+# < Less Than
+# >= Greater than or equal to
+# <= Less than or equal to
+# == Equal to
+# != Not equal to
+
+# Conditional Operators
+# if, else, elif
